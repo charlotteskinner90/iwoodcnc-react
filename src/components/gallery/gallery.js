05afa326dadde0bed095fc79957cb155b64a4a15
Charlotte Skinner <cs.skinner90@hotmail.co.uk>
2019-12-19 07:29:48 +0000

    WIP gallery

--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -7,8 +7,114 @@ export default class Gallery extends Component {
           <div className="row">
             <div className="twelve columns collapsed">
               <h1>Gallery</h1>
+              <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+                <div className="columns portfolio-item">
+                  <div className="item-wrap">
+                    <a href="#modal-01">
+                      <img src="images/wood.jpeg" className="item-img"/>
+                      <div className="overlay">
+                        <div className="portfolio-item-meta">
+                          <h5>Image</h5>
+                          <p>Image test</p>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+                <div className="columns portfolio-item">
+                  <div className="item-wrap">
+                    <a href="#modal-01">
+                      <img src="images/wood.jpeg" className="item-img"/>
+                      <div className="overlay">
+                        <div className="portfolio-item-meta">
+                          <h5>Image</h5>
+                          <p>Image test</p>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+                <div className="columns portfolio-item">
+                  <div className="item-wrap">
+                    <a href="#modal-01">
+                      <img src="images/wood.jpeg" className="item-img"/>
+                      <div className="overlay">
+                        <div className="portfolio-item-meta">
+                          <h5>Image</h5>
+                          <p>Image test</p>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+                <div className="columns portfolio-item">
+                  <div className="item-wrap">
+                    <a href="#modal-01">
+                      <img src="images/wood.jpeg" className="item-img"/>
+                      <div className="overlay">
+                        <div className="portfolio-item-meta">
+                          <h5>Image</h5>
+                          <p>Image test</p>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+                <div className="columns portfolio-item">
+                  <div className="item-wrap">
+                    <a href="#modal-01">
+                      <img src="images/wood.jpeg" className="item-img"/>
+                      <div className="overlay">
+                        <div className="portfolio-item-meta">
+                          <h5>Image</h5>
+                          <p>Image test</p>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+                <div className="columns portfolio-item">
+                  <div className="item-wrap">
+                    <a href="#modal-01">
+                      <img src="images/wood.jpeg" className="item-img"/>
+                      <div className="overlay">
+                        <div className="portfolio-item-meta">
+                          <h5>Image</h5>
+                          <p>Image test</p>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+                <div className="columns portfolio-item">
+                  <div className="item-wrap">
+                    <a href="#modal-01">
+                      <img src="images/wood.jpeg" className="item-img"/>
+                      <div className="overlay">
+                        <div className="portfolio-item-meta">
+                          <h5>Image</h5>
+                          <p>Image test</p>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+                <div className="columns portfolio-item">
+                  <div className="item-wrap">
+                    <a href="#modal-01">
+                      <img src="images/wood.jpeg" className="item-img"/>
+                      <div className="overlay">
+                        <div className="portfolio-item-meta">
+                          <h5>Image</h5>
+                          <p>Image test</p>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div> {/* row End */}
+          </div>
         </section>
       </React.Fragment>
     )
